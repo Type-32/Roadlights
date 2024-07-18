75353694a845aaf00056531ce1ae30f042f21412
Type-32 <87076491+Type-32@users.noreply.github.com>
2024-07-18 12:27:39 +0800

    Init Commit

---
+++ src/client/java/cn/crtlprototypestudios/roadlights/client/RoadlightsClient.java
@@ -0,0 +1,10 @@
+package cn.crtlprototypestudios.roadlights.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RoadlightsClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/cn/crtlprototypestudios/roadlights/Roadlights.java
@@ -0,0 +1,10 @@
+package cn.crtlprototypestudios.roadlights;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Roadlights implements ModInitializer {
+
+    @Override
+    public void onInitialize() {
+    }
+}
